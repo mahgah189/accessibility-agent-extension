f0828a7593a486d47f074031150e6bb16a767c05
Mark Baek <markbaek@mac.mynetworksettings.com>
2026-01-29 16:38:26 -0500

    added elevenlabs agent embed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import Popup from "/src/components/Popup.jsx";
+import Popup from "/src/components/Popup/Popup.jsx";
 
 function App() {
   const [count, setCount] = useState(0)
